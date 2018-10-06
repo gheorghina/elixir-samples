4f03a2290259113e9e7fb4794a5a842f0bf994ab
gheorghina.gligor <gheorghinab@yahoo.com>
2018-10-06 09:58:13 +0300

    word push

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,5 +6,29 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+
+    w_list =  sentence
+              |> String.downcase()
+              |> String.replace(~r/[,.:!@$%^&]/, "")
+              |> String.replace("  ", " ")
+              |> String.split(~r/[_\ ]/)
+
+    countw(w_list)
+
+  end
+
+  defp countw([], w_map) do
+    w_map
+  end
+
+  defp countw([head | tail], w_map \\ %{}) do
+    count = w_map
+            |> Map.get(head, 0)
+
+    w_map = w_map
+            |> Map.merge(%{ head => 1 + count})
+
+    countw(tail, w_map)
+
   end
 end
