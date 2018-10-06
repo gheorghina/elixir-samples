d0f517a7eaa1b29fbe3b07388c576b32d8c3c6ee
gheorghina.gligor <gheorghinab@yahoo.com>
2018-10-06 13:35:53 +0300

    word count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -4,24 +4,27 @@ defmodule Words do
 
   Words are compared case-insensitively.
   """
+  @punctuation_regex ~r/[,.:!@$%^&]/
+  @split_clause_regex ~r/[_\ ]/ 
+
   @spec count(String.t()) :: map
   def count(sentence) do
 
     w_list =  sentence
               |> String.downcase()
-              |> String.replace(~r/[,.:!@$%^&]/, "")
+              |> String.replace(@punctuation_regex, "")
               |> String.replace("  ", " ")
-              |> String.split(~r/[_\ ]/)
+              |> String.split( @split_clause_regex)
 
     countw(w_list)
 
   end
 
+  defp countw(words, w_map \\ %{})
   defp countw([], w_map) do
     w_map
   end
-
-  defp countw([head | tail], w_map \\ %{}) do
+  defp countw([head | tail], w_map) do
     count = w_map
             |> Map.get(head, 0)
 
