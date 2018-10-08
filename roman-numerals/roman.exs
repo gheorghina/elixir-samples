116e9cf8f24172af402b380a561aaa41a4078a4e
Ancuta Gheorghina Gligor <ggligor@roc01-eljg5j.ucverse.com>
2018-10-08 19:38:04 +0300

    small update

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -4,48 +4,47 @@ defmodule Roman do
   """
 
   @numbers_representation %{ 1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M" }
-  @limits [10]
+  # @limits [10]
 
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
 
-    @limits
-    |> Enum.map(fn n -> do_parse_no(n, number) end)
-    |> Enum.join()
+    cond do
+      number <= 10 -> roman_for_mod(number, 10)
+      number <= 40 -> do_parse_no(number, 10)
+    end
+
+    # @limits
+    # |> Enum.map(fn n -> do_parse_no(n, number) end)
+    # |> Enum.join()
 
   end
 
-  defp do_parse_no(margin, number) do
-
-    div = round(:math.floor(number/margin))
-    mod = round(:math.fmod(number, margin))
-
-    r_div = roman_for_div(div, margin)
-    r_mod = roman_for_mod(mod)
+  defp do_parse_no(number, margin) do
+    r_div = roman_for_div(number, margin)
+    r_mod = roman_for_mod(number, margin)
 
     "#{r_div}#{r_mod}"
-
   end
 
-  defp roman_for_div(number, margin) do
+  defp roman_for_mod(number, margin) do
+    mod = round(:math.fmod(number, margin))
+
     cond do
-      number == 0 -> ""
-      number < 10 -> do_get_values(number, margin)
+      mod == 0 -> ""
+      mod <= 10 -> @numbers_representation[mod]
     end
   end
 
-  defp do_get_values(no, margin) do
+  defp roman_for_div(number, margin) do
+    div = round(:math.floor(number/margin))
 
-    1..no
-    |> Enum.map(fn n -> @numbers_representation[margin] end)
-    |> Enum.join
-
-  end
-
-  defp roman_for_mod(number) do
     cond do
-      number == 0 -> ""
-      number <= 10 -> @numbers_representation[number]
+      div == 0 -> ""
+      div < 10 ->
+        1..div
+        |> Enum.map(fn n -> @numbers_representation[margin] end)
+        |> Enum.join
     end
   end
 
