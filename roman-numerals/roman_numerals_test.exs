9f22b09f67954cfa74277e5a7099dbb313ab5827
gheorghina.gligor <gheorghinab@yahoo.com>
2018-10-12 12:01:14 +0300

    adjusted roman draft

--- roman-numerals/roman_numerals_test.exs
+++ roman-numerals/roman_numerals_test.exs
@@ -5,6 +5,19 @@ end
 ExUnit.start()
 ExUnit.configure(exclude: :pending, trace: true)
 
+
+#
+# X, XX, XXX, XL, L, LX, LXX, LXXX, XC, C
+# C, CC, CCC, CD, D, DC, DCC, DCCC, CM, M
+# 39 = "Thirty nine" (XXX+IX) = XXXIX.
+# 246 = "Two hundred and forty six" (CC+XL+VI) = CCXLVI.
+# 1776 (M+DCC+LXX+VI) = MDCCLXXVI (the date written on the book held by the Statue of Liberty).[5]
+# 1954 (M+CM+L+IV) = MCMLIV (as in the trailer for the movie The Last Time I Saw Paris)[6]
+# 1990 (M+CM+XC) = MCMXC (used as the title of musical project Enigma's debut album MCMXC a.D., named after the year of its release).
+# 2014 (MM+X+IV) = MMXIV (the year of the games of the XXII (22nd) Olympic Winter Games (in Sochi)
+# The current year (2018) is MMXVIII.
+
+
 defmodule RomanTest do
   use ExUnit.Case
 
