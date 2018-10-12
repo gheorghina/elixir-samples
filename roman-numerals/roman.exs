c1921af0cadd6b98a7c36b6ccaa318f457e067fb
gheorghina.gligor <gheorghinab@yahoo.com>
2018-10-12 12:38:54 +0300

    roman no - closer to final solution

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -12,56 +12,38 @@ defmodule Roman do
   def numerals(number) do 
     
     @division_margins
-    |> Enum.map(fn m -> do_no(number, m)  end)
+    |> Enum.filter(fn m -> (number >= m and number < m * 10) or ( m == 10 and number < m) end)
+    |> Enum.map(fn m -> do_no(number, m) end)
     |> Enum.join 
 
   end
 
   defp do_no(number, margin) do
-
+    div = round(:math.floor(number/margin))
+    mod = round(:math.fmod(number, margin))    
+    
     r_mod = cond do
-          margin == 10 -> do_mod(number, margin) 
-          margin != 10 -> ""       
-        end
-
+              mod == 0 -> ""       
+              margin != 10 and mod > margin / 10 -> do_no(number, margin/10)
+              margin == 10 -> @numbers_representation[mod]
+            end
+    
     r_div = cond do
-          number >= margin and number < margin * 10 -> do_div(number, margin)
-          number < margin -> ""       
-        end
+              div == 0 -> "" 
+              div <= 3 -> do_repetive(div, margin)
+              div == 4 -> "#{@numbers_representation[margin]}#{@numbers_representation[margin * 5]}"
+              div == 5 -> "#{@numbers_representation[margin * 5]}"    
+              div > 5 and div <= 8 -> "#{ @numbers_representation[margin * 5]}#{do_repetive(div, margin)}"  
+              div == 9 -> "#{ @numbers_representation[margin]}#{ @numbers_representation[margin * 10]}"              
+            end
 
     "#{r_div}#{r_mod}"
   end
-
-  defp do_mod(number, margin) do
-    mod = round(:math.fmod(number, margin))
-
-    cond do
-      mod == 0 -> ""      
-      mod != 0 -> @numbers_representation[mod]
-    end
-  end
-
-  defp do_div(number, margin) do
-    div = round(:math.floor(number/margin))
-    r_margin_mid_upper = @numbers_representation[margin * 5] 
-    r_margin_upper =  @numbers_representation[margin * margin] 
-    r_margin =  @numbers_representation[margin] 
-
-    cond do
-      div == 0 -> ""     
-      div <= 3 -> do_repetive(div, margin)
-      div == 4 -> "#{r_margin}#{r_margin_mid_upper}"
-      div == 5 -> "#{r_margin_mid_upper}"
-      div > 5 and div <= 8 -> "#{r_margin_mid_upper}#{do_repetive(div, margin)}"
-      div == 9 -> "#{r_margin}#{r_margin_upper}" 
-    end
-  end 
 
   defp do_repetive(div, margin) do
      1..div
      |> Enum.map(fn n -> @numbers_representation[margin] end)
      |> Enum.join 
-
   end 
 
 end
