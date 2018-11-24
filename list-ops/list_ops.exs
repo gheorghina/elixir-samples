56e071b9745137a2e326d585c9c86600e50f4258
Ancuta Gheorghina Gligor <gheorghina.gligor@8x8.com>
2018-11-24 17:50:21 +0200

    raw list operations

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -29,23 +29,43 @@ defmodule ListOps do
   @spec map(list, (any -> any)) :: list
   def map(l, f), do: reverse(map(l, f, []))
 
-  defp map([], f, acc), do: acc
+  defp map([], _, acc), do: acc
 
   defp map([head|tail], f, acc) do
     map(tail, f, [f.(head) | acc])
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter(l, f), do: reverse(filter(l, f,  []))
+
+  defp filter([], _, acc), do: acc
+
+  defp filter([head| tail], f, acc) do
+   if f.(head) do
+    filter(tail, f, [head | acc])
+    else
+      filter(tail, f, acc)
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
+  def reduce([], acc, _), do: acc
+
+  def reduce([head | tail], acc, f) do
+    reduce(tail, f.(head, acc), f)
   end
 
   @spec append(list, list) :: list
+
   def append(a, b) do
+    acc = append_to_acc(reverse(b), [])
+    append_to_acc(reverse(a), acc)
+  end
+
+  def append_to_acc([], acc), do: acc
+  def append_to_acc([head | tail], acc) do
+    append_to_acc(tail, [head | acc])
   end
 
   @spec concat([[any]]) :: [any]
