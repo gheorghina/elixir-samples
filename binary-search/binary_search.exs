eafdb21ecf900bbaf6032d8714113e59d07b7b2e
Ancuta Gheorghina Gligor <gheorghina.gligor@8x8.com>
2019-02-04 18:20:47 +0200

    simple binary search

--- binary-search/binary_search.exs
+++ binary-search/binary_search.exs
@@ -16,8 +16,40 @@ defmodule BinarySearch do
       {:ok, 2}
 
   """
+  @not_found :not_found
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
+    search_in_tuple(numbers |> Tuple.to_list(), key)
+  end
+
+  defp search_in_tuple(numbers, key) do
+    list_len = numbers |> length()
+    last_elem = numbers |> List.last()
+
+    cond do
+      (last_elem == nil) or (last_elem < key) or ((list_len == 1) and (last_elem != key)) -> @not_found
+      last_elem == key -> {:ok, list_len - 1}
+      true ->
+        split_len = list_len/2 |> round
+        [list_1, list_2] = numbers |> Enum.chunk(split_len, split_len,[])
+
+        if list_1 |> is_in_list(key) do
+          search_in_tuple(list_1, key)
+        else
+          if list_2 |> is_in_list(key) do
+            search_in_tuple(list_2, key)
+          end
+        end
+    end
+  end
+
+  defp is_in_list(list, key) do
+    last_elem = list |> List.last()
+    if last_elem >= key do
+      true
+    else
+      false
+    end
   end
 end
