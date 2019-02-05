357882a3ecc3ab21438b4d60b25de614e6951983
Ancuta Gheorghina Gligor <gheorghina.gligor@8x8.com>
2019-02-05 13:05:44 +0200

    small updates

--- binary-search/binary_search.exs
+++ binary-search/binary_search.exs
@@ -26,10 +26,13 @@ defmodule BinarySearch do
   defp search_in_tuple(numbers, key, idx \\ 0) do
     list_len = numbers |> length()
     last_elem = numbers |> List.last()
+    last_elem_position = idx + (list_len - 1)
 
     cond do
-      (last_elem == nil) or (last_elem < key) or ((list_len == 1) and (last_elem != key)) -> @not_found
-      last_elem == key -> {:ok, idx + (list_len - 1)}
+      last_elem == nil -> @not_found
+      last_elem < key -> @not_found
+      list_len == 1 and last_elem != key -> @not_found
+      last_elem == key -> {:ok, last_elem_position}
       true ->
         split_len = list_len/2 |> round
         [list_1, list_2] = numbers |> Enum.chunk(split_len, split_len,[])
