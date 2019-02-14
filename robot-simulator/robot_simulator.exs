7c3c6ca5d22317febac5546b91be8b37cc1b7099
Ancuta Gheorghina Gligor <gheorghina.gligor@8x8.com>
2019-02-14 17:14:37 +0200

    first draft in adding robot simulator mechanisms

--- robot-simulator/robot_simulator.exs
+++ robot-simulator/robot_simulator.exs
@@ -4,8 +4,43 @@ defmodule RobotSimulator do
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
+
+  defstruct([:direction, :position])
+
+  @allowed_directions [:north, :south, :east, :west]
+
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(direction \\ :north, position \\ {0, 0}) do
+
+    case position |> validate_position() do
+      {:ok, position} ->
+        case direction |> validate_direction() do
+          {:ok, direction} -> %RobotSimulator{direction: direction, position: position}
+            error -> error
+        end
+        error -> error
+    end
+  end
+
+  defp validate_position(position) when is_tuple(position) and :erlang.tuple_size(position) == 2 and  is_integer(elem(position, 0)) and is_integer(elem(position, 1)) do
+    {:ok, position}
+  end
+
+  defp validate_position(position) do
+    {:error, "invalid position"}
+  end
+
+  defp validate_direction(direction) when is_atom(direction)  do
+
+    if Enum.member?(@allowed_directions, direction) do
+      {:ok, direction}
+    else
+      {:error, "invalid direction"}
+    end
+  end
+
+  defp validate_direction(direction) do
+    {:error, "invalid direction"}
   end
 
   @doc """
@@ -24,6 +59,12 @@ defmodule RobotSimulator do
   """
   @spec direction(robot :: any) :: atom
   def direction(robot) do
+
+    case robot do
+      %RobotSimulator{direction: direction} -> direction
+      _ -> nil
+    end
+
   end
 
   @doc """
@@ -31,5 +72,11 @@ defmodule RobotSimulator do
   """
   @spec position(robot :: any) :: {integer, integer}
   def position(robot) do
+
+    case robot do
+      %RobotSimulator{position: position} -> position
+      _ -> nil
+    end
+
   end
 end
